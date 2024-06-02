9190fdcd7117efacb39ee5751261b6c19c6df1c8
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2024-06-02 15:01:18 +0200

    Configure path in scripts

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+# Configure Path
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 # Vars
 PLAYBOOK_NAME=cleanup
 VERBOSITY=-v # 0-5
 
 # Create directory
 DATE=$(date +%Y-%m-%d)
-mkdir -p $ANSIBLE_LOGS/$PLAYBOOK_NAME/$DATE
+mkdir -p /logs/$PLAYBOOK_NAME/$DATE
 
 # Create log
 TIME=$(date +%H-%M-%S)
-LOGFILE=$ANSIBLE_LOGS/$PLAYBOOK_NAME/$DATE/$TIME.log
+LOGFILE=/logs/$PLAYBOOK_NAME/$DATE/$TIME.log
 
 # Launch Ansible Playbook
 ansible-playbook $VERBOSITY \
--- scripts/hello-world.sh
+++ scripts/hello-world.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+# Configure Path
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 # Vars
 PLAYBOOK_NAME=hello-world
 VERBOSITY=-vvvv # 0-5
+
+# Create directory
+DATE=$(date +%Y-%m-%d)
+mkdir -p /logs/$PLAYBOOK_NAME/$DATE
+
+# Create log
+TIME=$(date +%H-%M-%S)
+LOGFILE=/logs/$PLAYBOOK_NAME/$DATE/$TIME.log
 
 # Launch Ansible Playbook
 ansible-playbook $VERBOSITY \
 -i $ANSIBLE_INVENTORY \
 $ANSIBLE_PLAYBOOKS/$PLAYBOOK_NAME.yaml \
---vault-password-file $VAULT_PASSWORD_FILE
+--vault-password-file $VAULT_PASSWORD_FILE \
+>> $LOGFILE 2>&1
